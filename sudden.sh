2d44e36f599f4d031f84812e01b92ea229b5428b
eokxw <478win@gmail.com>
2024-07-11 10:24:32 +0900

    deploy 2

--- sudden.sh
+++ sudden.sh
@@ -5,10 +5,11 @@ FILE=~/tmp/_SUDDEN
 while true
 do
 	if [[ -f "$FILE" ]]; then
-	#echo "check $FILE"
-	echo "SUDDEN!!!"
-else
-	clear
+		#echo "check $FILE"
+		echo "SUDDEN!!!"
+	else
+		clear
+		date "+%Y-%M-%d"
 	fi
 	sleep 3
 done
